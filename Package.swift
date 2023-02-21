e93c7228fb6ec63a4aa979eeac2493a25d67591b
Casey <casey@popprika.com>
2023-02-21 11:35:31 +0900

    Add package sources

--- Package.swift
+++ Package.swift
@@ -2,27 +2,25 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
 let package = Package(
+    // 2
     name: "PetSaveOnboarding",
+    // 3
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
+    // 4
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PetSaveOnboarding",
-            targets: ["PetSaveOnboarding"]),
+            targets: ["PetSaveOnboarding"]
+        )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    // 5
+    dependencies: [],
+    // 6
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PetSaveOnboarding",
-            dependencies: []),
-        .testTarget(
-            name: "PetSaveOnboardingTests",
-            dependencies: ["PetSaveOnboarding"]),
+            resources: [.copy("Resources/Assets.xcassets")]
+        )
     ]
 )
